173602823a94383e4b10f1c9c58ee0929460a980
joaquingutierrez <joaquinguty@gmail.com>
2023-04-30 10:34:19 -0300

    Agregados los manejadores de eventos para el input y el boton

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, TextInput, Button } from 'react-native';
-import {styles} from "./style"
+import { View, TextInput, Button } from 'react-native';
+import { styles } from "./style"
+import { useState } from 'react';
+
+
 
 export default function App() {
+    const [eventList, setEventList] = useState([])
+    const [eventText, setEventText] = useState("") 
+    
+    const handleOnChangeText = (e) => {
+        return setEventText(e)
+    }
+    const handleAddEvent = () => {
+        if (eventText.length > 0) {
+            setEventList([
+                ...eventList,
+                eventText
+            ])
+            setEventText("")
+        }
+    }
     return (
         <View style={styles.container}>
             <View style={styles.inputContainer}>
-            <TextInput placeholder='Nuevo evento...' style={styles.input}/>
-            <Button title="Agregar" color="#0077B6" />
+                <TextInput placeholder='Nuevo evento...' style={styles.input} onChangeText={handleOnChangeText} value={eventText} />
+                <Button title="Agregar" color="#0077B6" onPress={handleAddEvent} />
             </View>
         </View>
     );
