f1f30296744eb15574e20d1c301b9ff1d6cbeaec
joaquingutierrez <joaquinguty@gmail.com>
2023-05-24 15:39:25 -0300

    Mejoras visuales en el tab

--- src/navigation/tabs/index.jsx
+++ src/navigation/tabs/index.jsx
@@ -1,7 +1,8 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import { Ionicons } from '@expo/vector-icons';
 
 import { ShopNavigation, CartNavigation, OrdersNavigation } from "../index";
+import { colors } from "../../constants/theme";
 
 
 const BottomTab = createBottomTabNavigator()
@@ -9,10 +10,41 @@ const BottomTab = createBottomTabNavigator()
 const TabsNavigator = () => {
 
     return (
-        <BottomTab.Navigator>
-            <BottomTab.Screen name="ShopTab" component={ShopNavigation} />
-            <BottomTab.Screen name="CartTab" component={CartNavigation} />
-            <BottomTab.Screen name="OrdersTab" component={OrdersNavigation} />
+        <BottomTab.Navigator
+            initialRouteName="ShopTab"
+            screenOptions={{ 
+                headerShown: false,
+                tabBarLabelStyle: {
+                    fontSize: 14,
+                },
+                tabBarActiveTintColor: colors.secundary,
+                tabBarInactiveTintColor: colors.tertiary,
+                tabBarStyle: {
+                    backgroundColor: colors.primary
+                },
+            }}
+        >
+            <BottomTab.Screen name="ShopTab" component={ShopNavigation}
+                options={{ 
+                    tabBarLabel: "Shop",
+                    tabBarIcon: ({focused, color, size}) => {
+                        return <Ionicons name="home" size={size} color={color} />
+                    }
+            }} />
+            <BottomTab.Screen name="CartTab" component={CartNavigation}
+                options={{ 
+                    tabBarLabel: "Cart",
+                    tabBarIcon: ({focused, color, size}) => {
+                        return <Ionicons name="cart" size={size} color={color} />
+                    } 
+                }} />
+            <BottomTab.Screen name="OrdersTab" component={OrdersNavigation}
+                options={{ 
+                    tabBarLabel: "Orders",
+                    tabBarIcon: ({focused, color, size}) => {
+                        return <Ionicons name="book" size={size} color={color} />
+                    }
+            }} />
         </BottomTab.Navigator>
     )
 }
