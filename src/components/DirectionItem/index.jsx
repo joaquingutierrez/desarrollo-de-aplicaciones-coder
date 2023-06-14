8335c63c86cb0ea6f50fe901b1663fc51d3fd91a
joaquingutierrez <joaquinguty@gmail.com>
2023-06-14 15:05:18 -0300

    Solucionado problema con los titulos de las direcciones en DirectionsScreen

--- src/components/DirectionItem/index.jsx
+++ src/components/DirectionItem/index.jsx
@@ -2,13 +2,13 @@ import { View, TouchableOpacity, Image, Text } from "react-native";
 
 import {styles} from "./style"
 
-const DirectionItem = ({id, direction, image, onSelect}) => {
+const DirectionItem = ({id, title, image, onSelect}) => {
 
     return (
         <TouchableOpacity style={styles.container} onPress={onSelect}>
             <Image style={styles.image} source={{uri: image}} />
             <View style={styles.info}>
-                <Text style={styles.title}>{direction}</Text>
+                <Text style={styles.title}>{title}</Text>
             </View>
         </TouchableOpacity>
     )
